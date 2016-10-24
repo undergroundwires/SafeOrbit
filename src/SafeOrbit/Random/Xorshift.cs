244c9dcca69af64cad807b10a5c5dd7bd6d7eb09
undergroundwires <undergroundwires@safeorb.it>
2016-10-24 18:42:47 +0200

    license headers are added to SafeOrbit

--- src/SafeOrbit/Random/Xorshift.cs
+++ src/SafeOrbit/Random/Xorshift.cs
@@ -1,4 +1,29 @@
-﻿using System;
+﻿
+/*
+MIT License
+
+Copyright (c) 2016 Erkin Ekici - undergroundwires@safeorb.it
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -10,7 +35,7 @@ namespace SafeOrbit.Random
 {
     /// <summary>
     /// This random alghoritm is around 29x times faster than <see cref="System.Random"/>.
-    /// This alghoritm has no crypthological value and is predictable.
+    /// This alghoritm has no cryptological value and is predictable.
     /// </summary>
     public class Xorshift : RandomNumberGenerator
     {
