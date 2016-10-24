1e25a15e1f1875a9d138f2ee2a61840ef07ebd18
undergroundwires <undergroundwires@safeorb.it>
2016-10-24 20:47:55 +0200

    extract public key and token now extracts the public key in the text file as well

--- tools/extract public key and token.bat
+++ tools/extract public key and token.bat
@@ -4,16 +4,18 @@
 @cls
 
 REM Fill the following
-set outputSnkName= REM UnitTests-public-key.snk
-set outputTokenName= REM UnitTests-public-key-token.txt
-set filePath= REM ..\docs\signature_keys\private\UnitTests-key.snk
+set outputSnkName=REM IntegrationTests-public-key.snk
+set outputTxtName=REM IntegrationTests-public-key.txt
+set filePath=REM ..\docs\signature_keys\private\IntegrationTests-key.snk
 
 set sn="C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\sn.exe"
-
+set local=%~dp0
 REM Extract Public Key from filePath and write it to outputName
 %sn% -p %filePath% %outputSnkName%
 
-REM Extract Publick Key Token from Token.snk and write it to PublicToken.txt
-%sn% -t %outputSnkName% > %outputTokenName%
+REM Public key : This outputs a very long key over five lines. Copy it and add the lines together (e.g. via notepad)
+REM Public key token : This outputs  public key token as well.
+%sn% -tp %outputSnkName% > %outputTxtName%
 
+ 
 pause
