77e5e2d5ad43bfff1f668dd8f966dd65d09ec66b
undergroundwires <undergroundwires@safeorb.it>
2016-10-29 01:43:11 +0200

    Random is moved to Core project

---
+++ src/SafeOrbitNetCore/Cryptography/Random/RandomGenerator/EntropySources/SystemRng.cs
@@ -0,0 +1,70 @@
+﻿
+/*
+MIT License
+
+Copyright (c) 2016 Erkin Ekici - undergroundwires@safeorb.it
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+using System;
+using System.Security.Cryptography;
+
+namespace SafeOrbit.Random.Tinhat
+{
+    /// <summary>
+    /// This is just a wrapper around <see cref="RNGCryptoServiceProvider" /> and adds no value 
+    /// whatsoever, except to serve as a placeholder in the "EntropySources" folder, just to remember to consider
+    /// RNGCryptoServiceProvider explicitly as an entropy source.
+    /// </summary>
+    /// <seealso cref="RandomNumberGenerator" />
+    public sealed class SystemRng : RandomNumberGenerator
+    {
+        private readonly RandomNumberGenerator _systemRngProvider;
+        public bool IsDisposed { get; private set; }
+        public SystemRng() : this(new RNGCryptoServiceProvider()) { }
+        /// <exception cref="ArgumentNullException"><paramref name="systemRngProvider"/> is <see langword="null" />.</exception>
+        internal SystemRng(RandomNumberGenerator systemRngProvider)
+        {
+            if (systemRngProvider == null) throw new ArgumentNullException(nameof(systemRngProvider));
+            _systemRngProvider = systemRngProvider;
+        }
+        public override void GetBytes(byte[] data)
+        {
+            _systemRngProvider.GetBytes(data);
+        }
+#if NET46
+        public override void GetNonZeroBytes(byte[] data)
+        {
+            _systemRngProvider.GetNonZeroBytes(data);
+        }
+#endif
+        protected override void Dispose(bool disposing)
+        {
+            if (this.IsDisposed) return;
+            _systemRngProvider.Dispose();
+            base.Dispose(disposing);
+            this.IsDisposed = true;
+        }
+        ~SystemRng()
+        {
+            Dispose(false);
+        }
+    }
+}
