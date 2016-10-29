832475a3edb4a8646fff3d9c8dd7beed4c7d094d
undergroundwires <undergroundwires@safeorb.it>
2016-10-29 15:21:56 +0200

    - hash is renamed to Hashers. - moving InjectionDetector to Core

---
+++ src/SafeOrbitNetCore/Memory/InjectionDetector/Stamper/IlCodeStamper.cs
@@ -0,0 +1,59 @@
+﻿
+/*
+MIT License
+
+Copyright (c) 2016 Erkin Ekici - undergroundwires@safeorb.it
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+using System;
+using System.Linq;
+using SafeOrbit.Hash;
+
+namespace SafeOrbit.Memory.InjectionServices.Stampers
+{
+    /// <summary>
+    ///     Stamps <see cref="Type" /> of an object by serializing its IL-code.
+    /// </summary>
+    /// <seealso cref="StamperBase{Type}" />
+    /// <seealso cref="IStamper{Type}" />
+    internal class IlCodeStamper : StamperBase<Type>
+    {
+        public static IlCodeStamper StaticInstance = new IlCodeStamper();
+
+        public IlCodeStamper() : this(Murmur32.StaticInstance)
+        {
+        }
+
+        internal IlCodeStamper(IFastHasher fastHasher) : base(fastHasher)
+        {
+        }
+
+        public override InjectionType InjectionType { get; } = InjectionType.CodeInjection;
+
+        protected override byte[] GetSerializedBytes(Type @object)
+        {
+            var result = @object.GetMethods().Select(m => m.GetMethodBody())
+                .Where(m => m != null)
+                .SelectMany(m => m.GetILAsByteArray()).ToArray();
+            return result;
+        }
+    }
+}
---
+++ src/SafeOrbitNetCore/Memory/InjectionDetector/Stamper/StamperBase.cs
@@ -0,0 +1,54 @@
+﻿
+/*
+MIT License
+
+Copyright (c) 2016 Erkin Ekici - undergroundwires@safeorb.it
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+using System;
+using SafeOrbit.Hash;
+using SafeOrbit.Memory.Injection;
+
+namespace SafeOrbit.Memory.InjectionServices.Stampers
+{
+    internal abstract class StamperBase<TObject>: IStamper<TObject>
+    {
+        protected abstract byte[] GetSerializedBytes(TObject @object);
+        public abstract InjectionType InjectionType { get; }
+        private readonly IFastHasher _fastHasher;
+        protected StamperBase(IFastHasher fastHasher)
+        {
+            _fastHasher = fastHasher;
+        }
+        protected int Hash(byte[] input) => _fastHasher.ComputeFast(input);
+        private int SerializeAndHash(TObject @object)
+        {
+            var serialization = GetSerializedBytes(@object);
+            var hash = _fastHasher.ComputeFast(serialization);
+            return hash;
+        }
+        public IStamp<int> GetStamp(TObject @object)
+        {
+            var hash = SerializeAndHash(@object);
+            return new Stamp(hash);
+        }
+    }
+}
