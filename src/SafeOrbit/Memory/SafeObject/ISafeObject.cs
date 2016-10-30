0cc80a4a84558f9f9192c3da80f8f228e5c4e04f
undergroundwires <undergroundwires@safeorb.it>
2016-10-30 17:07:40 +0100

    fixed lines

--- src/SafeOrbit/Memory/SafeObject/ISafeObject.cs
+++ src/SafeOrbit/Memory/SafeObject/ISafeObject.cs
@@ -1,5 +1,4 @@
-﻿
-/*
+﻿/*
 MIT License
 
 Copyright (c) 2016 Erkin Ekici - undergroundwires@safeorb.it
@@ -31,39 +30,43 @@ using SafeOrbit.Memory.InjectionServices;
 namespace SafeOrbit.Memory
 {
     /// <summary>
-    /// Abstracts an object that can detect the injections to itself.
+    ///     Abstracts an object that can detect the injections to itself.
     /// </summary>
     /// <typeparam name="TObject">The type of the the class.</typeparam>
     /// <seealso cref="Common.IProtectionLevelSwitchProvider{SafeObjectProtectionMode}" />
     /// <seealso cref="IAlerts" />
     /// <seealso cref="IDisposable" />
+    /// <seealso cref="SafeObjectProtectionMode" />
     public interface ISafeObject<out TObject> :
-        IProtectionLevelSwitchProvider<SafeObjectProtectionMode>,
-        IAlerts,
-        IDisposable
+            IProtectionLevelSwitchProvider<SafeObjectProtectionMode>,
+            IAlerts,
+            IDisposable
         where TObject : class
     {
         /// <summary>
-        /// Gets a value indicating whether this instance is modifiable.
+        ///     Gets a value indicating whether this instance is modifiable.
         /// </summary>
         /// <value><c>true</c> if this instance is modifiable; otherwise, <c>false</c>.</value>
-        /// <seealso cref="MakeReadOnly"/>
+        /// <seealso cref="MakeReadOnly" />
         bool IsReadOnly { get; }
+
         /// <summary>
-        /// Gets the object.
+        ///     Gets the object.
         /// </summary>
         /// <value>The object.</value>
         TObject Object { get; }
+
         /// <summary>
-        /// Closes this instance to any kind of changes.
+        ///     Closes this instance to any kind of changes.
         /// </summary>
-        /// <seealso cref="IsReadOnly"/>
+        /// <seealso cref="IsReadOnly" />
         void MakeReadOnly();
+
         /// <summary>
-        /// Verifies the last changes on the object.
+        ///     Verifies the last changes on the object.
         /// </summary>
-        /// <exception cref="ReadOnlyAccessForbiddenException">This instance of <see cref="TObject"/> is marked as ReadOnly.</exception>
-        /// <seealso cref="IsReadOnly"/>
+        /// <exception cref="ReadOnlyAccessForbiddenException">This instance of <see cref="TObject" /> is marked as ReadOnly.</exception>
+        /// <seealso cref="IsReadOnly" />
         /// <param name="modification">The modification.</param>
         void ApplyChanges(Action<TObject> modification);
     }
