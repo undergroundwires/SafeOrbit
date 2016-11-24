c885602f985160442b95a874c1bfe6d9a13fc534
undergroundwires <undergroundwires@safeorb.it>
2016-11-24 21:10:24 +0100

    nuget logic is updated. it's now automated and the package will be created after building on "release"

--- tools/create nuget package.bat
+++ tools/create nuget package.bat
@@ -1,11 +1,11 @@
 REM -- Project variables
 set sourceDir=..\src\SafeOrbit
-set projectFileName=SafeOrbit.csproj
+set projectFileName=SafeOrbit.xproj
 REM -- Nuget
 set local=%~dp0
 set nuget=%local%nuget.exe
 
 cd %sourceDir%
 
-%nuget% pack SafeOrbit.csproj -Symbols -Prop Configuration=Release
+%nuget% pack %projectFileName% -Prop Configuration=Release
  pause
--- tools/create nuget specs file.bat
+++ tools/create nuget specs file.bat
@@ -1,6 +1,6 @@
 REM --------- Prepare
 REM -- Project variables
-set sourceDir=..\src\AsyncWindowsClipboard
+set sourceDir=..\src\SafeOrbit.xproj
 REM -- Nuget
 set local=%~dp0
 set nuget=%local%nuget.exe
