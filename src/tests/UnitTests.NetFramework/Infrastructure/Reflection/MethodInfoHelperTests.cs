a4ada62941e0593f212b241cd3974071e761f1f2
undergroundwires <undergroundwires@safeorb.it>
2016-11-26 14:18:49 +0100

    Fixed UnitTests:NetFramework not building

---
+++ src/tests/UnitTests.NetFramework/Infrastructure/Reflection/MethodInfoHelperTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace SafeOrbit.Infrastructure.Reflection
+{
+    /// <seealso cref="MethodInfoHelper"/>
+    [TestFixture]
+    public class MethodInfoHelperTests
+    {
+        [Test]
+        public void GetIlBytes_ArgumentIsNull_throwsArgumentNullException()
+        {
+            //arrange
+            var nullArgument = (MethodInfo) null;
+            //act
+            TestDelegate callingWithNull = () => nullArgument.GetIlBytes();
+            //assert
+            Assert.That(callingWithNull, Throws.ArgumentNullException);
+        }
+        [Test]
+        public void GetIlBytes_TwiceForSameType_ReturnsSame()
+        {
+            //arrange
+            var type = typeof(string);
+            //act
+            var expected = type.GetMethods().Select(m => m.GetIlBytes());
+            var actual = type.GetMethods().Select(m => m.GetIlBytes());
+            //assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+        [Test]
+        public void GetIlBytes_ForDifferentTypes_ReturnsDifferent()
+        {
+            //arrange
+            var type = typeof(string);
+            var differentType = typeof(int);
+            //act
+            var expected = type.GetMethods().Select(m => m.GetIlBytes());
+            var actual = differentType.GetMethods().Select(m => m.GetIlBytes());
+            //assert
+            Assert.That(actual, Is.Not.EqualTo(expected));
+        }
+    }
+}
--- src/tests/UnitTests.NetFramework/Memory/SafeString/SafeStringToStringMarshalerTests.cs
+++ src/tests/UnitTests.NetFramework/Memory/SafeString/SafeStringToStringMarshalerTests.cs
@@ -25,6 +25,8 @@ SOFTWARE.
 
 using System;
 using NUnit.Framework;
+using SafeOrbit.Fakes;
+using SafeOrbit.UnitTests;
 
 namespace SafeOrbit.Memory
 {
