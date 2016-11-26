8f96daa68d433c4cf0c1ec11cc19511fd6ff1aa5
undergroundwires <undergroundwires@safeorb.it>
2016-11-26 15:00:42 +0100

    default nuget export path is changed, loggings are added, change for parameter structure

--- src/SafeOrbit/makeNuget.cmd
+++ src/SafeOrbit/makeNuget.cmd
@@ -1,7 +1,17 @@
 @echo off
 REM %1	: Current configuration
 REM %2	: Requested configuration (will only create package on this mode)
-REM %3	: Requested configuration (will only create package on this mode)
+REM %3  : Target destination
+
+echo ------Nuget variables-------
+echo Current configuration   : %1
+echo Requested configuration : %2
+echo Target destination : %3
+echo ----------------------------
 
 
-IF "%1" == "%2" dotnet pack --no-build --configuration %1 -o ../%3
+IF "%1" == "%2" (
+	dotnet pack --no-build --configuration %1 -o %3
+) ELSE (
+	echo Nuget package is not created as current configuration does not match the requested configuration.
+)
