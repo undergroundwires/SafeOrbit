ab0ebb023bf20315ff2f963ce63aaf8db2fb553b
undergroundwires <undergroundwires@safeorb.it>
2016-12-01 23:49:05 +0100

    commented

--- src/SafeOrbit/Memory/SafeBytes/SafeByte/DataProtection/MemoryProtector.NetCore.cs
+++ src/SafeOrbit/Memory/SafeBytes/SafeByte/DataProtection/MemoryProtector.NetCore.cs
@@ -30,28 +30,34 @@ using SafeOrbit.Cryptography.Random;
 
 namespace SafeOrbit.Memory.SafeBytesServices.DataProtection
 {
+    /// <summary>
+    ///     .NET Core implementation of <see cref="IByteArrayProtector" />.
+    /// </summary>
+    /// <seealso cref="IByteArrayProtector" />
     public partial class MemoryProtector : IByteArrayProtector
     {
-        public int BlockSizeInBytes => Encryptor.BlockSize;
-
         private static readonly IFastEncryptor Encryptor = new BlowfishEncryptor(BlowfishCipherMode.Ecb);
         private readonly IFastEncryptor _encryptor;
         private readonly byte[] _key;
 
-        public MemoryProtector() : this(Encryptor, FastRandom.StaticInstance) { }
+        public MemoryProtector() : this(Encryptor, FastRandom.StaticInstance)
+        {
+        }
+
         internal MemoryProtector(IFastEncryptor encryptor, ICryptoRandom random)
         {
             _encryptor = encryptor;
             _key = random.GetBytes(Encryptor.MinKeySize);
         }
 
+        public int BlockSizeInBytes => Encryptor.BlockSize;
+
         public void Protect(byte[] userData)
         {
             if (userData == null) throw new ArgumentNullException(nameof(userData));
-                EnsureParameter(userData);
             var encryptedBytes = _encryptor.Encrypt(userData, _key);
             SetBytesToByteArray(
-                source:encryptedBytes,
+                source: encryptedBytes,
                 target: ref userData);
         }
 
@@ -60,18 +66,19 @@ namespace SafeOrbit.Memory.SafeBytesServices.DataProtection
             if (encryptedData == null) throw new ArgumentNullException(nameof(encryptedData));
             var decryptedBytes = _encryptor.Decrypt(encryptedData, _key);
             SetBytesToByteArray(
-                  source: decryptedBytes,
-                  target: ref encryptedData);
+                source: decryptedBytes,
+                target: ref encryptedData);
         }
 
         private static void SetBytesToByteArray(ref byte[] target, byte[] source)
         {
-            if(target.Length != source.Length) throw new ArgumentException($"Length of the {nameof(target)} must be equal to the length of {nameof(source)}. {nameof(target)} length was {target.Length}, {nameof(source)} length is {source.Length}");
+            if (target.Length != source.Length)
+                throw new ArgumentException(
+                    $"Length of the {nameof(target)} must be equal to the length of {nameof(source)}. {nameof(target)} length was {target.Length}, {nameof(source)} length is {source.Length}");
             for (var i = 0; i < source.Length; i++)
-            {
                 target[i] = source[i];
-            }
         }
     }
 }
+
 #endif
