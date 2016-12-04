1b2f16b2dec74d789a59d8229a4a06237bec59ab
undergroundwires <undergroundwires@safeorb.it>
2016-12-04 01:31:54 +0100

    thread-testing is moved to integration tests

---
+++ src/tests/UnitTests/Cryptography/Random/RandomBaseTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.Linq;
+
+namespace SafeOrbit.Cryptography.Random
+{
+    /// <seealso cref="RandomBase" />
+    [TestFixture]
+    public abstract class RandomBaseTests
+    {
+
+    }
+}
