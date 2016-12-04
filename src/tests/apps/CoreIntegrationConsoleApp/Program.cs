be17f10e1f2f46d73fb802f19c2d5c766968f382
undergroundwires <undergroundwires@safeorb.it>
2016-12-04 23:32:36 +0100

    a little core integration app is written.

---
+++ src/tests/apps/CoreIntegrationConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using CoreIntegrationConsoleApp.Tests;
+using SafeOrbit.Cryptography.Encryption;
+using SafeOrbit.Library;
+using SafeOrbit.Memory;
+
+namespace CoreIntegrationConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            SafeOrbitCore.Current.StartEarly();
+            var tests = GetAllTests();
+            foreach (var test in tests)
+            {
+                test.RunTest();
+                Console.WriteLine("----------------------");
+            }
+            Console.ReadKey();
+        }
+
+        private static IEnumerable<ITest> GetAllTests()
+        {
+            yield return new BlowfishEncryptorTest();
+            yield return new SafeStringTest();
+        }
+    }
+}
