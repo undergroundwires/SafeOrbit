942f25ea017bce15b950fffa441cedb86a0e9fb3
undergroundwires <undergroundwires@users.noreply.github.com>
2018-08-23 20:02:35 +0200

    copyright update
    from 2016 to 2018

--- src/SafeOrbit/Cryptography/Encryption/Blowfish/Algorithm/BlowfishEcb.cs
+++ src/SafeOrbit/Cryptography/Encryption/Blowfish/Algorithm/BlowfishEcb.cs
@@ -2,7 +2,7 @@
 /*
 MIT License
 
-Copyright (c) 2016 Erkin Ekici - undergroundwires@safeorb.it
+Copyright (c) 2018 Erkin Ekici - undergroundwires@safeorb.it
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
