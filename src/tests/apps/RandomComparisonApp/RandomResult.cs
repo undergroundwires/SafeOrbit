432ae72a0e59051bee87fa0045f93705dd7604f9
undergroundwires <undergroundwires@users.noreply.github.com>
2019-03-16 14:55:22 +0100

    refactorings & clean up in RandomComparisonApp

--- src/tests/apps/RandomComparisonApp/RandomResult.cs
+++ src/tests/apps/RandomComparisonApp/RandomResult.cs
@@ -4,8 +4,17 @@ namespace RandomComparisonApp
 {
     public class RandomResult
     {
-        public string AlgorithmName;
-        public double CompressionRatio;
-        public TimeSpan TimeSpan;
+        public string AlgorithmName { get; }
+        public double CompressionRatio { get; }
+        public TimeSpan TimeSpan { get; }
+        public RandomResult(string algorithmName, double compressionRatio, TimeSpan timeSpan)
+        {
+            if (string.IsNullOrEmpty(algorithmName))
+                throw new ArgumentException("Value cannot be null or empty.", nameof(algorithmName));
+            if (compressionRatio <= 0) throw new ArgumentOutOfRangeException(nameof(compressionRatio));
+            AlgorithmName = algorithmName;
+            CompressionRatio = compressionRatio;
+            TimeSpan = timeSpan;
+        }
     }
 }
