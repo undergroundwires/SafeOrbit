629a86a713aa9c88aae4ba5eda92489a887c78b2
undergroundwires <undergroundwires@users.noreply.github.com>
2019-03-16 13:12:30 +0100

    removed versioning as it's not necessary

--- src/tests/PerformanceTests/Properties/AssemblyInfo.cs
+++ src/tests/PerformanceTests/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("26608a1a-0cce-4d06-9011-f455a9095594")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.*")]
-[assembly: AssemblyFileVersion("0.1.*")]
