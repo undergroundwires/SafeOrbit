305cf3da6117820a94bd0fde6377536240e14142
undergroundwires <undergroundwires@users.noreply.github.com>
2019-03-16 13:13:02 +0100

    copyright year update

--- src/tests/UnitTests.NetFramework/Properties/AssemblyInfo.cs
+++ src/tests/UnitTests.NetFramework/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("UnitTests.NetFramework")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © 2016 - 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
