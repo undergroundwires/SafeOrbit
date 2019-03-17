039752515e8984d95b29d61ef8509d28eb8b3fc6
undergroundwires <undergroundwires@users.noreply.github.com>
2019-03-17 02:33:32 +0100

    multi-threading test

--- src/tests/IntegrationTests/Memory/SafeString/SafeStringTests.cs
+++ src/tests/IntegrationTests/Memory/SafeString/SafeStringTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Threading;
+using NUnit.Framework;
 
 namespace SafeOrbit.Memory
 {
@@ -15,6 +16,26 @@ namespace SafeOrbit.Memory
             sut.Append("t");
             sut.Append("es");
             sut.Append('t');
+            using (var sm = new SafeStringToStringMarshaler(sut))
+            {
+                var actual = sm.String;
+                Assert.That(actual, Is.EqualTo(expected));
+            }
+        }
+
+        [Test]
+        public void SafeString_ModifiedByAnotherThread_ReturnsAppendedText()
+        {
+            var expected = "test";
+            var sut = new SafeString();
+            sut.Append("t");
+            var thread = new Thread(() =>
+            {
+                sut.Append("es");
+                sut.Append('t');
+            });
+            thread.Start();
+            thread.Join();
             using (var sm = new SafeStringToStringMarshaler(sut))
             {
                 var actual = sm.String;
