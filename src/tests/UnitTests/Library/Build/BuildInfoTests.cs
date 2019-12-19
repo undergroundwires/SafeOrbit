5ee8815d50a10c1af38370bf6aaa63f685a362f8
undergroundwires <undergroundwires@users.noreply.github.com>
2019-12-19 03:08:54 +0100

    added tests that were mistakenly ignored by gitignore

---
+++ src/tests/UnitTests/Library/Build/BuildInfoTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace SafeOrbit.Library.Build
+{
+    /// <see cref="IBuildInfo"/>
+    /// <see cref="BuildInfo"/>
+    [TestFixture]
+    public class BuildInfoTests
+    {
+        [Test]
+        public void TargetPlatform_Is_NetCore()
+        {
+            const Platform expected = Platform.NetStandard2;
+            var sut = new BuildInfo();
+            var actual = sut.TargetPlatform;
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
