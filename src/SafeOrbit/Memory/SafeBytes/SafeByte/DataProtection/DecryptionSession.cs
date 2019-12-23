594b849a8ee962672153267415fe030ebe6422d5
undergroundwires <undergroundwires@users.noreply.github.com>
2019-12-24 00:29:01 +0100

    refactorings

--- src/SafeOrbit/Memory/SafeBytes/SafeByte/DataProtection/DecryptionSession.cs
+++ src/SafeOrbit/Memory/SafeBytes/SafeByte/DataProtection/DecryptionSession.cs
@@ -6,14 +6,13 @@ namespace SafeOrbit.Memory.SafeBytesServices.DataProtection
     public class DecryptionSession : IDecryptionSession
     {
         private readonly IByteArrayProtector _protector;
-        private readonly byte[] _decryptedBytes;
         public DecryptionSession(IByteArrayProtector protector, ref byte[] encryptedBytes)
         {
             _protector = protector;
-            _decryptedBytes = encryptedBytes;
-            protector.Protect(_decryptedBytes);
+            PlainBytes = encryptedBytes;
+            protector.Protect(PlainBytes);
         }
-        public void Dispose() => _protector.Protect(_decryptedBytes);
-        public byte[] PlainBytes => _decryptedBytes;
+        public void Dispose() => _protector.Protect(PlainBytes);
+        public byte[] PlainBytes { get; }
     }
 }
