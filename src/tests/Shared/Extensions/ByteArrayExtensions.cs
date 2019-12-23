81e3ff086f6024b68afd1a84450a284b526ce439
undergroundwires <undergroundwires@users.noreply.github.com>
2019-12-24 00:31:09 +0100

    added extension method for tests to quickly create new byte array from the source

---
+++ src/tests/Shared/Extensions/ByteArrayExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Shared.Extensions
+{
+    public static class ByteArrayExtensions
+    {
+        public static byte[] CopyToNewArray(this byte[] byteArray)
+        {
+            var newArray = new byte[byteArray.Length];
+            Array.Copy(byteArray, newArray, byteArray.Length);
+            return newArray;
+        }
+    }
+}
