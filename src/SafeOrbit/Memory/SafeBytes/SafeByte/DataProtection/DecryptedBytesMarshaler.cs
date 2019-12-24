334ce1fadc8fcf0704796a3f46d22c49a80d425f
undergroundwires <undergroundwires@users.noreply.github.com>
2019-12-24 08:21:44 +0100

    documented exceptions

--- src/SafeOrbit/Memory/SafeBytes/SafeByte/DataProtection/DecryptedBytesMarshaler.cs
+++ src/SafeOrbit/Memory/SafeBytes/SafeByte/DataProtection/DecryptedBytesMarshaler.cs
@@ -4,9 +4,10 @@ namespace SafeOrbit.Memory.SafeBytesServices.DataProtection
 {
     public class DecryptedBytesMarshaler : IDecryptedBytesMarshaler
     {
-        private volatile bool _isDisposed = false;
+        private volatile bool _isDisposed;
         private readonly byte[] _plainBytes;
 
+        /// <exception cref="ObjectDisposedException" accessor="get">If object is disposed</exception>
         public byte[] PlainBytes
         {
             get
@@ -16,18 +17,24 @@ namespace SafeOrbit.Memory.SafeBytesServices.DataProtection
             }
         }
 
+        /// <exception cref="ArgumentException">Throws if the <paramref name="decryptedBytes" /> is empty.</exception>
+        /// <exception cref="ArgumentNullException">Throws if the <paramref name="decryptedBytes" /> is null.</exception>
         public DecryptedBytesMarshaler(byte[] decryptedBytes)
         {
             if (decryptedBytes == null) throw new ArgumentNullException(nameof(decryptedBytes));
-            if (decryptedBytes.Length == 0) throw new ArgumentException("Value cannot be an empty collection.", nameof(decryptedBytes));
-            this._plainBytes = decryptedBytes;
+            if (decryptedBytes.Length == 0)
+                throw new ArgumentException("Value cannot be an empty collection.", nameof(decryptedBytes));
+            _plainBytes = decryptedBytes;
         }
+
+        /// <exception cref="ObjectDisposedException">If object is already disposed</exception>
         public void Dispose()
         {
             EnsureNotDisposed();
             Array.Clear(PlainBytes, 0, PlainBytes.Length);
             _isDisposed = true;
         }
+
         private void EnsureNotDisposed()
         {
             if (_isDisposed)
