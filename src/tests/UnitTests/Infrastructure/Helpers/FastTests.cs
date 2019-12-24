82fbb55a821477565dad80cb8404d59e50dcb2ce
undergroundwires <undergroundwires@users.noreply.github.com>
2019-12-24 19:38:51 +0100

    Unit tests for Fast.For

---
+++ src/tests/UnitTests/Infrastructure/Helpers/FastTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Concurrent;
+using System.Linq;
+using NUnit.Framework;
+using SafeOrbit.Helpers;
+
+namespace UnitTests.Infrastructure.Helpers
+{
+    /// <seealso cref="Fast"/>
+    [TestFixture]
+    public class FastTests
+    {
+        [Test]
+        public void For_ZeroToTen_EachIterationIsCalled()
+        {
+            // Arrange
+            var iterations = new ConcurrentBag<int>();
+            var expected = new [] { 0,1,2,3,4,5,6,7,8,9};
+            // Act
+            Fast.For(0, 10, 
+                i => iterations.Add(i)
+                );
+            // Assert
+            var actual = iterations.OrderBy(p => p).ToArray();
+            Assert.True(expected.SequenceEqual(actual));
+        }
+    }
+}
