3e85dd2dede907ab91a47991ebf9008e0b86e844
undergroundwires <undergroundwires@users.noreply.github.com>
2020-01-30 04:01:20 +0100

    better docs 📝

--- src/SafeOrbit/Common/IAsyncEquatable.cs
+++ src/SafeOrbit/Common/IAsyncEquatable.cs
@@ -2,11 +2,14 @@
 
 namespace SafeOrbit.Common
 {
-    /// <summary>Defines a generalized method that a value type or class implements to create a type-specific method for determining equality of instances.</summary>
+    /// <summary>
+    ///     Defines a generalized method that a value type or class implements to create a type-specific method for
+    ///     determining equality of instances.
+    /// </summary>
     /// <typeparam name="T">The type of objects to compare.</typeparam>
     public interface IAsyncEquatable<in T>
     {
-        /// <summary>Indicates whether the current object is equal to another object of the same type.</summary>
+        /// <summary>Indicates whether the current object is equal to another object of the <typeparamref name="T" /> type.</summary>
         /// <param name="other">An object to compare with this object.</param>
         /// <returns>true if the current object is equal to the <paramref name="other">other</paramref> parameter; otherwise, false.</returns>
         Task<bool> EqualsAsync(T other);
